cb3af7727121a5a4b6d1c68863c35aefdc3817ae
peterclermy <peterclermy232@gmail.com>
2022-08-14 13:37:09 +0300

    commit tree d5a55b85b6c903db000dbaa3ac69b9b1c8638f13

---
+++ .history/conference_badges_20220814133157.rb
@@ -0,0 +1,7 @@
+# Write your code here.
+
+def badge_maker(name = "Arel")
+    "Hello, my name is #{name}."
+end
+def batch_badge_creator(name = "Arel", name = "Carol")
+    ["Hello, my name is #{name}.",]
---
+++ .history/conference_badges_20220814133543.rb
@@ -0,0 +1,7 @@
+# Write your code here.
+
+def badge_maker(name = "Arel")
+    "Hello, my name is #{name}."
+end
+
+
